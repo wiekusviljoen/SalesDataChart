c4cda99cc8fab8c6315d230288643a82c6388182
wiekusviljoen <wiekusviljoen@gmail.com>
2023-02-13 12:45:22 +0200

    commit on master

--- SalesDataChart/Controllers/SalesRecordsController.cs
+++ SalesDataChart/Controllers/SalesRecordsController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesDataChart.DataLayer;
+using System.Data.Common;
 
 namespace SalesDataChart.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly DbContextSales _context;
+
+        public SalesRecordsController(DbContextSales context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +21,23 @@ namespace SalesDataChart.Controllers
         public IActionResult ShowSalesData() 
         { 
             return View();
+        }
+
+        [HttpPost]
+
+        public List<object> GetSalesData()
+        {
+            List<object> data = new List<object>();
+
+            List<string> labels = _context.SalesData.Select(p=>p.Monthname).ToList();
+
+            data.Add(labels);
+
+            List<int> SalesNumber = _context.SalesData.Select(p => p.TotalSales).ToList();
+
+            data.Add(SalesNumber);
+            
+            return data;
         }
     }
 }
